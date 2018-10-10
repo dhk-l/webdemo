afb27aca75ab0aba2a0c8236e4f2ea764f175c71
18147 <181471308@qq.com>
2018-10-10 12:10:14 +0800

    test2

---
+++ src/com/hk/idea/com/hk/idea/Test2.java
@@ -0,0 +1,11 @@
+package com.hk.idea.com.hk.idea;
+
+/**
+ * @author LTP
+ * @create 2018-10-10 12:09
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Test2.main");
+    }
+}
