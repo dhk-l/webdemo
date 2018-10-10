d52d0b57a98b3b6c889f42ef357d3cd35d09f7c5
18147 <181471308@qq.com>
2018-10-10 12:06:45 +0800

    test1

---
+++ src/com/hk/idea/com/hk/idea/Test.java
@@ -0,0 +1,11 @@
+package com.hk.idea.com.hk.idea;
+
+/**
+ * @author LTP
+ * @create 2018-10-10 11:59
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test.main");
+    }
+}
---
+++ src/com/hk/idea/com/hk/idea/User.java
@@ -0,0 +1,17 @@
+package com.hk.idea.com.hk.idea;
+
+/**
+ * @author LTP
+ * @create 2018-10-10 1:07
+ */
+public class User {
+    private int age;
+
+    public void setUserAge(){
+        age = 11;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
